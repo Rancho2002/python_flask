a845d6f54c41c4b3aa0b7a322c22c3c704b4359c
arijit ghosh <subhasghosh196@outlook.com>
2023-07-25 17:38:35 +0530

    done with v3, v2 is best

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+# Configure the Extension
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todolists.db"
+db.init_app(app)
 
-@app.route("/products")
-def product():
-    return "Product pages"
+
+# Define Models
+class Todo(db.Model):
+    sl = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    subject = db.Column(db.String(500),nullable=False)
+    date_created=db.Column(db.DateTime, default=date.today())
+
+    def __repr__(self) -> str:
+        return f"{self.sl} - {self.title}"
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/",methods=['GET','POST'])
+def index():
+
+    if request.method== "POST":
+        title=request.form['title']
+        subject=request.form['subject']
+        todo=Todo(title=title,subject=subject)
+        db.session.add(todo)
+        db.session.commit()
+
+    # query data
+    allTodo=Todo.query.all() #v2.0.x
+    # track=len(list(allTodo))
+
+    return render_template("index.html",alllist=allTodo)
+
+@app.route("/delete/<int:serial>")
+def delete(serial):
+    item= db.session.execute(db.select(Todo).filter_by(sl=serial)).scalar_one()
+    db.session.delete(item)
+    db.session.commit()
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
